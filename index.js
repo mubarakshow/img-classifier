c4e693935d341a4f5e83807d86eeba497dfa6894
Mubarak Show <mubarak.show@gmail.com>
2019-09-01 20:53:27 +0100

    added 4th classifier

--- index.js
+++ index.js
@@ -22,20 +22,25 @@ async function app() {
   document.getElementById('class-a').addEventListener('click', () => addExample(0));
   document.getElementById('class-b').addEventListener('click', () => addExample(1));
   document.getElementById('class-c').addEventListener('click', () => addExample(2));
+  document.getElementById('class-d').addEventListener('click', () => addExample(3));
 
   while (true) {
     if (classifier.getNumClasses() > 0) {
+
+      console.log('getNumClasses:', classifier.getNumClasses())
       // Get the activation from mobilenet from the webcam
       const activation = net.infer(webcamElement,  'conv_preds');
       // Get the most likely class and confidence from the calssifier module 
       const result = await classifier.predictClass(activation);
 
-      const classes = ['A', 'B', 'C'];
+      const classes = ['A', 'B', 'C', 'No Action!'];
+
       document.getElementById('console').innerText = `
         prediction: ${classes[result.classIndex]}\n
         probability: ${result.confidences[result.classIndex]}
       `;
     }
+     
     await tf.nextFrame();
   }
 }
